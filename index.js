21ef8faef19f31289ab2c4518425ab72e3157618
ShukshinaMI <74698961+ShukshinaMI@users.noreply.github.com>
2023-01-13 11:32:52 +0500

    add factory-method

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const Factory = function () {
+  this.createEmployee = function (name, type) {
+    let employee = null;
+
+    switch (type) {
+      case 'fulltime': {
+        employee = new FullTime();
+        break;
+      }
+      case 'parttime': {
+        employee = new PartTime();
+        break;
+      }
+      case 'temporary': {
+        employee = new Temporary();
+        break;
+      }
+      case 'contractor': {
+        employee = new Contractor();
+        break;
+      }
+      default:
+        null;
+    }
+
+    employee.name = name;
+    employee.type = type;
+
+    employee.say = function () {
+      console.log(
+        `I am employee ${this.name}. ${this.type}: rate ${this.hourly}/hour`
+      );
+    };
+
+    return employee;
+  };
+};
+
+const FullTime = function () {
+  this.hourly = '$12';
+};
+
+const PartTime = function () {
+  this.hourly = '$11';
+};
+
+const Temporary = function () {
+  this.hourly = '$10';
+};
+
+const Contractor = function () {
+  this.hourly = '$15';
+};
+
+function runAcquaintance() {
+  const employees = [];
+  const factory = new Factory();
+
+  employees.push(
+    factory.createEmployee('Masha', 'fulltime'),
+    factory.createEmployee('Dasha', 'parttime'),
+    factory.createEmployee('Pasha', 'temporary'),
+    factory.createEmployee('Sasha', 'contractor')
+  );
+
+  employees.forEach((employee) => {
+    employee.say();
+  });
+}
+
+runAcquaintance();
